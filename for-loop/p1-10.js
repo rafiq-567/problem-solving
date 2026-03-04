5c8ecebf2323b990c18ffa52861dbb3df3809da2
unknown <mdrafiqulakhter0@gmail.com>
2026-03-05 00:18:15 +0600

    solved p3

--- for-loop/p1-10.js
+++ for-loop/p1-10.js
@@ -4,7 +4,14 @@
 // }
 
 // Problem-2
-for (let i=10; i >=1; i--) {
-    console.log(i)
+// for (let i=10; i >=1; i--) {
+//     console.log(i)
+// }
+
+//problem-3
+for (let i=1; i <=50; i++){
+    if (i%2==0){
+        console.log(i)
+    }
 }
 
