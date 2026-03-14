d2958ef288abe234573189353d1bb359760552e4
unknown <mdrafiqulakhter0@gmail.com>
2026-03-15 00:28:37 +0600

    found the big number

--- for-loop/21-30.js
+++ for-loop/21-30.js
@@ -14,9 +14,25 @@
 // console.log(result);
 
 // p-23
-for (let i = 1; i <= 20; i++) {
-  if (i % 5 === 0) {
-    continue;
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 5 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// p-24
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
   }
-  console.log(i);
+
+  return max;
 }
+
+const numbers = [10, 5, 8, 20, 15];
+console.log(findMax(numbers))
