e21ca20088e84695da30784896e840481c353d2d
unknown <mdrafiqulakhter0@gmail.com>
2026-03-14 23:21:41 +0600

    solved star problem

--- for-loop/21-30.js
+++ for-loop/21-30.js
@@ -1,3 +1,14 @@
-for (let i = 65; i <= 90; i++) {
-  console.log(String.fromCharCode(i) + " : " + i);
+// p-21
+// for (let i = 65; i <= 90; i++) {
+//   console.log(String.fromCharCode(i) + " : " + i);
+// }
+
+// p-22
+let n = 5;
+let result = "";
+
+for (let i = 1; i <= n; i++) {
+  result += "*";
 }
+
+console.log(result);
