079a3998b62daffc9a9c85229598165b5be17e34
unknown <mdrafiqulakhter0@gmail.com>
2026-03-21 23:55:04 +0600

    sorted checked

--- for-loop/31-40.js
+++ for-loop/31-40.js
@@ -1,8 +1,18 @@
 // p-31
 
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-const arr = [1, 2, 2, 3, 4, 4, 5];
+// function removeDuplicates(arr) {
+//   return [...new Set(arr)];
+// }
+// const arr = [1, 2, 2, 3, 4, 4, 5];
 
-console.log(removeDuplicates(arr));
+// console.log(removeDuplicates(arr));
+
+// p-32
+function isSorted(arr) {
+  const asc = arr.every((val, i) => i === 0 || arr[i - 1] <= val);
+  const desc = arr.every((val, i) => i === 0 || arr[i - 1] >= val);
+
+  return asc || desc;
+}
+const arr = [ 1, 2, 3, 4, 2];
+console.log(isSorted(arr));    
