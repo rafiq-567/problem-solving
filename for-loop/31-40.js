d2d54add44568ef82fc1542410846d1cc11a4594
unknown <mdrafiqulakhter0@gmail.com>
2026-03-25 21:51:29 +0600

    solved p-34

--- for-loop/31-40.js
+++ for-loop/31-40.js
@@ -18,14 +18,22 @@
 // console.log(isSorted(arr));    
 
 // p-33
-function findMin(arr){
-  let min=arr[0]
-  for (let i = 1; i<arr.length; i++){
-    if(arr[i]<min){
-      min =arr[i]
-    }
-  }
-  return min
+// function findMin(arr){
+//   let min=arr[0]
+//   for (let i = 1; i<arr.length; i++){
+//     if(arr[i]<min){
+//       min =arr[i]
+//     }
+//   }
+//   return min
+// }
+// const numbers = [5, 2, 9, 1, 7]
+// console.log(findMin(numbers))
+
+// p-34
+const string = (str)=>{
+  const reverse = str.split("").reverse().join("");
+  return reverse===str
 }
-const numbers = [5, 2, 9, 1, 7]
-console.log(findMin(numbers))
+const result = string("madam")
+console.log(result)
