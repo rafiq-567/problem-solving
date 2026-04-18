5d16ed0f3fc00872af2feed939dbe0d6cad751b4
unknown <mdrafiqulakhter0@gmail.com>
2026-04-19 02:03:37 +0600

    solved p-43

--- for-loop/41-50.js
+++ for-loop/41-50.js
@@ -17,19 +17,54 @@
 // console.log(capitalizeWords("hello world from javascript")); 
 
 // p-42
-function sumDigitsInString(str) {
-  let sum = 0;
+// function sumDigitsInString(str) {
+//   let sum = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
 
-    if (char >= '0' && char <= '9') {
-      sum += Number(char);
-    }
+//     if (char >= '0' && char <= '9') {
+//       sum += Number(char);
+//     }
+//   }
+
+//   return sum;
+// }
+
+// const input = "abc123def45";
+// console.log("Total Sum:", sumDigitsInString(input));
+
+
+// p-43
+function areAnagrams(str1, str2) {
+  // If lengths differ, they cannot be anagrams
+  if (str1.length !== str2.length) {
+    return false;
   }
 
-  return sum;
+  const charMap = {};
+
+  // First Loop: Count characters in the first string
+  for (let i = 0; i < str1.length; i++) {
+    const char = str1[i];
+    charMap[char] = (charMap[char] || 0) + 1;
+  }
+
+  // Second Loop: Subtract counts based on the second string
+  for (let i = 0; i < str2.length; i++) {
+    const char = str2[i];
+
+    // If the character doesn't exist in map or count is 0, it's not an anagram
+    if (!charMap[char]) {
+      return false;
+    }
+    
+    charMap[char]--;
+  }
+
+  return true;
 }
 
-const input = "abc123def45";
-console.log("Total Sum:", sumDigitsInString(input));
+// Example usage:
+console.log(areAnagrams("listen", "silent")); // Output: true
+console.log(areAnagrams("hello", "world"));   // Output: false
