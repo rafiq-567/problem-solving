8209b55a3b20c07d4689fd4c807a6db97df447a0
unknown <mdrafiqulakhter0@gmail.com>
2026-05-03 14:35:04 +0600

    solved usingmath.max

--- js/100.js
+++ js/100.js
@@ -8,12 +8,26 @@
 // console.log(addTwo(2,5))
 
 // 3
-function evenOdd(){
-    for(let i = 1; i<=100; i++)
-        if(i%2===0){
-            return "even"
-        }else{
-            return "odd"
-        }
-}
+// function evenOdd(){
+//     for(let i = 1; i<=100; i++)
+//         if(i%2===0){
+//             return "even"
+//         }else{
+//             return "odd"
+//         }
+// }
+
+// 4
+// function largestOfThree(a, b, c) {
+//     if (a >= b && a >= c){
+//         return a
+//     }else if(b>=a&&b>=c){
+//         return b
+//     }else{
+//         return c
+//     }
+// }
+
+const largest = Math.max(10, 25, 66)
+console.log(largest)
 
