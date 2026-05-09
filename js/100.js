a5985810b3aad5707efcee3420b1bbe6090afe64
unknown <mdrafiqulakhter0@gmail.com>
2026-05-09 19:41:30 +0600

    basic calculator logic

--- js/100.js
+++ js/100.js
@@ -33,6 +33,28 @@
 
 
 // 5
-for (i=1; i <=10; i++){
-    console.log(i)
+// for (i=1; i <=10; i++){
+//     console.log(i)
+// }
+
+// 6
+function calculate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return b !== 0 ? a / b : "Cannot divide by zero";
+        default:
+            return "Invalid operator";
+    }
 }
+
+
+console.log(calculate(10, 5, '+')); 
+console.log(calculate(10, 5, '-'));
+console.log(calculate(10, 5, '*'));
+console.log(calculate(10, 5, '/'));
